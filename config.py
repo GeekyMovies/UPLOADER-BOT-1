9b1d7c3069924bfad56d406fc3e9b411a35381f2
GeekyMovies <121683432+GeekyMovies@users.noreply.github.com>
2023-01-12 22:01:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,33 @@
 import os
 
 class Config(object):
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    # Get these values from my.telegram.org
-    # the download location, where the HTTP Server runs
+    
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5915563603:AAE1STI93wsA7MK9pma-MOKXU4Gww5KxwWk")
+    
+    API_ID = int(os.environ.get("API_ID", 23311160))
+    
+    API_HASH = os.environ.get("API_HASH", "2a1366013eca4256bce853346dbcda49")
+    
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
+    
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 4194304000 #2097152000
+
+    TG_MAX_FILE_SIZE = 2097152000
+
     FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
+    
     CHUNK_SIZE = int(128)
-    # default thumbnail to be used in the videos
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+
     HTTP_PROXY = ""
-    # maximum message length in Telegram
+    
     MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
+    
     PROCESS_MAX_TIMEOUT = 3600
-    # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "UPLOADER-X-BOT"
-    # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5691018873"))
+
+    SESSION_NAME = "UploadLinkToFileBot"
+    
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Geekymovies:Geekymovies@cluster0.7llffit.mongodb.net/?retryWrites=true&w=majority")
+
     MAX_RESULTS = "50"
-    PREMIUM_USER = os.environ.get("PREMIUM_USER")
